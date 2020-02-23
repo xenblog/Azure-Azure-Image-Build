eeaa5449143fc51e37ca366c21a683cf7e314460
Niels Holm <nch@edgemo.com>
2020-02-23 15:50:49 +0100

    master

--- AzureImageBuilder.ps1
+++ AzureImageBuilder.ps1
@@ -48,5 +48,5 @@ Invoke-AzResourceAction -ResourceGroupName $AIBResourceGroup -ResourceType Micro
 # Support 
 Get-AzVMImagePublisher -Location "East US" | Select PublisherName
 Get-AzVMImageOffer -Location "East US" -PublisherName "MicrosoftWindowsDesktop" | Select Offer
-Get-AzVMImageSku -Location "East US" -PublisherName "MicrosoftWindowsDesktop" -Offer "windows-10"
+Get-AzVMImageSku -Location "East US" -PublisherName "MicrosoftWindowsDesktop" -Offer "windows-10" | Select SKUs
 Get-AzVMImage -Location "East US" -PublisherName "MicrosoftWindowsDesktop" -Offer "windows-10" -Sku "19h2-evd" | Select Version 
